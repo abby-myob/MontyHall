6c833f8208566360b83fb738c0138b32ae0d50b6
Abby Thompson <abby.thompson@myob.com>
2019-08-27 16:53:21 +1200

    adding consoleResponeThingy

--- MontyHall/MontyHallLibrary/ConsoleResponseThingy.cs
+++ MontyHall/MontyHallLibrary/ConsoleResponseThingy.cs
@@ -1,7 +1,28 @@
+using System;
+using MontyHallLibrary.Interfaces;
+
 namespace MontyHallLibrary
 {
-    public class ConsoleResponseThingy
+    public class ConsoleResponseThingy : IResponseThingy
     {
-        
+        public int PickDoor()
+        {
+            Console.WriteLine("what door?");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public bool SwitchToOtherDoor()
+        {
+            Console.WriteLine("switch?");
+            var response = Console.ReadLine();
+            switch (response)
+            {
+                case "y":
+                case "Y":
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
